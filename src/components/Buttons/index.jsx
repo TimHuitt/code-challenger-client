d339a5ddff9e5fe8ab75fc32dd70692bd2f043f2
Tim Huitt <timhuitt@gmail.com>
2024-02-11 01:43:59 -0700

    add settings display

--- src/components/Buttons/index.jsx
+++ src/components/Buttons/index.jsx
@@ -5,49 +5,6 @@ import nextSvg from '../../../public/next.svg'
 
 const Buttons = () => {
   
-  const { requestData, setRequestData } = useStateContext();
-  const { setChallengeResponse } = useStateContext();
-  
-  
-  const sendRequest = async () => {
-    const url = "https://code-challenger-server-9e5cc705b6e9.herokuapp.com/challenges";
-    try {
-      const res = await fetch(url, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({content: requestData}),
-      });
-  
-      if (res.ok) {
-        const jsonData = await res.json();
-        return jsonData;
-      } else {
-        throw new Error("Invalid request!");
-      }
-    } catch (err) {
-      console.log(err.message);
-    }
-  };
-  
-  const getChallenge = async () => {
-    try {
-      const resData = await sendRequest();
-  
-      if (resData) {
-        if (typeof resData.response === 'string') {
-          setChallengeResponse(JSON.parse(resData.response))
-        } else {
-          setChallengeResponse(resData.response)
-        }
-      } else {
-        console.log('no data')
-      }
-    } catch (err) {
-      console.log(err);
-    }
-  };
 
   return (
     <div id="Buttons">
@@ -59,9 +16,6 @@ const Buttons = () => {
             <img src={nextSvg} />
           </button>
         </div>
-        {/* <div id="buttons-code">
-          <button>Give Up</button>
-        </div> */}
       </div>
     </div>
   )
--- src/components/Code/index.jsx
+++ src/components/Code/index.jsx
@@ -2,16 +2,22 @@ import { useState } from 'react'
 import CodeBox from '../CodeBox'
 import Console from '../Console'
 
+import { useStateContext } from '../../StateContext'
 import './Code.css'
 
 const Code = () => {
-
+  const { requestData } = useStateContext();
   const [ logData, setLogData ] = useState([''])
 
   return (
     <div id="Code">
       <CodeBox setLogData={setLogData} logData={logData} />
       <Console logData={logData} />
+      <div className="stats">
+        language: {requestData.language}<br />
+        difficulty: {requestData.difficulty}<br />
+        length: {requestData.length}
+      </div>
     </div>
   )
 }
