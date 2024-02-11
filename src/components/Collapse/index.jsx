0e95394d0f82ba1db53f4c1a9b1202ab554dcb3e
Tim Huitt <timhuitt@gmail.com>
2024-02-10 23:23:33 -0700

    add collapse button toggle

--- src/components/Collapse/index.jsx
+++ src/components/Collapse/index.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { useStateContext } from '../../StateContext'
 import './Collapse.css'
 
@@ -7,12 +7,23 @@ import collapseSvg from '/public/collapse.svg'
 const Collapse = () => {
 
   const { challengeToggle, setChallengeToggle } = useStateContext()
+  const [ rotation, setRotation ] = useState(180)
+
+  const handleClick = () => {
+    setChallengeToggle(prev => !prev)
+  }
+
+  useEffect(() => {
+    challengeToggle
+      ? setRotation(180)
+      : setRotation(0)
+  }, [challengeToggle])
 
   return (
     <div id="Collapse">
       <div className="vertical-ruler" />
       <div className="collapse-container">
-        <img src={collapseSvg} alt="collapse" />
+        <img src={collapseSvg} alt="collapse" style={{ transform: `rotate(${rotation}deg)` }} onClick={handleClick} />
       </div>
       <div className="vertical-ruler" />
     </div>
