c2f35bb2f9b1da246e8e5c0e26ccb23de8f1af55
Tim Huitt <timhuitt@gmail.com>
2026-06-27 17:12:30 -0600

    remove analytics

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,6 @@ import ReactDOM from "react-dom/client"
 import App from "./components/App"
 import { BrowserRouter as Router } from "react-router-dom"
 import { StateProvider } from './StateContext'
-import ReactGA from 'react-ga4'
-
-ReactGA.initialize("G-EVH9V7EX44")
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Router>
